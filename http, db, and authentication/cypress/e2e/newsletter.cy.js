80f5913d48a4f47721a90e2fd0b0ed3820f99326
Vladimir <vladimirg@moonsite.co.il>
2024-11-08 19:38:10 +0200

    init commit

---
+++ http, db, and authentication/cypress/e2e/newsletter.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="Cypress" />
+
+describe('Newsletter', () => {
+  beforeEach(() => {
+    cy.task('seedDatabase');
+  });
+
+  it('should display a success message', () => {
+    cy.intercept('POST', '/newsletter*', { body: { status: 201 } }).as('subscribe');
+
+    cy.visit('/');
+    cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
+    cy.contains('Thanks for signing up');
+  });
+
+  it('should display validation errors', () => {
+    cy.intercept('POST', '/newsletter*', { body: { message: 'Email exists already' } }).as('subscribe');
+
+    cy.visit('/');
+    cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
+    cy.contains('Email exists already');
+  });
+
+  it('should successfully create a new contact', () => {
+    cy
+      .request({
+        method: 'POST',
+        url: '/newsletter',
+        body: {
+          email: 'test@example.com'
+        },
+        form: true,
+      })
+      .then(response => {
+        expect(response.status).to.eq(201);
+      });
+  });
+});
