edc8dabd9953d111f6f8fd1a1f666c34969a3652
Vladimir <vladimirg@moonsite.co.il>
2024-11-08 20:52:42 +0200

    updated tests

--- http, db, and authentication/cypress/support/commands.js
+++ http, db, and authentication/cypress/support/commands.js
@@ -50,3 +50,13 @@ Cypress.on('uncaught:exception', (err) => {
     return false;
   }
 });
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/login');
+  cy.get('[data-cy="auth-email"]').click();
+  cy.get('[data-cy="auth-email"]').type('test@example.com');
+  cy.get('[data-cy="auth-password"]').type('testpassword');
+  cy.get('[data-cy="auth-submit"]').click();
+  cy.location('pathname').should('eq', '/takeaways')
+  cy.getCookie('__session').its('value').should('not.be.empty');
+});
